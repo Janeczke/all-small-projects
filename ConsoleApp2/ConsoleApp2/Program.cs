8e06df07b1d5c8733a4f0ac4556d3c8f294cec33
Szymon <111386002+Janeczke@users.noreply.github.com>
2023-01-19 17:29:16 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("    (^^)     (^^)    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine("    ||         ||    ");
+            Console.WriteLine(" (OOO)         (OOO) ");
+            Console.WriteLine(" (OOO)         (OOO) ");
+            Console.WriteLine(" |   |         |   | ");
+            Console.WriteLine(" |   |         |   | ");
+            Console.WriteLine(" |_  |_       _|  _| ");
+            Console.WriteLine(" _____________________ ");
+
+
+
+
+
+        }
+
+    }
+}
